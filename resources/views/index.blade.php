fa28e570a87b98247be12e5c3eeb980af68d65b7
Nguyễn Ngọc Văn <nguyenngocvan@BCTC_GROUP.local>
2021-04-26 14:21:37 +0700

    Frontpage Carousel

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,19 +1,81 @@
 @extends('layouts.app')
 @section('content')
 
-<div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="/" class="d-block w-100" alt="...">
-      </div>
-      <div class="carousel-item">
-        <img src="..." class="d-block w-100" alt="...">
-      </div>
-      <div class="carousel-item">
-        <img src="..." class="d-block w-100" alt="...">
+  <div class="slider" style="margin-top:-25px;">
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="/slider/slider1.png" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="/slider/slider2.png" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="/slider/slider3.png" class="d-block w-100" alt="...">
+        </div>
       </div>
     </div>
   </div>
 
-
+  <div class="container mt-5">
+    <span>
+      <h1>Xe hơi</h1>
+      <a href="#" class="float-right">Xem tất cả</a>
+    </span>
+    <div id="carouselExampleFade" class="carousel slide" data-ride="carousel" data-interval="2500">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <div class="row">
+            <div class="col-3">
+              <img src="/product/car1.jpg" class="img-thumbnail" alt="...">
+              <p class="text-center card-footer" style="color:#222;">Tên sản phẩm/5.000.000đ</p>
+            </div>
+            <div class="col-3">
+              <img src="/product/car1.jpg" class="img-thumbnail" alt="...">
+              <p class="text-center card-footer" style="color:#222;">Tên sản phẩm/5.000.000đ</p>
+            </div>
+            <div class="col-3">
+              <img src="/product/car1.jpg" class="img-thumbnail" alt="...">
+              <p class="text-center card-footer" style="color:#222;">Tên sản phẩm/5.000.000đ</p>
+            </div>
+            <div class="col-3">
+              <img src="/product/car1.jpg" class="img-thumbnail" alt="...">
+              <p class="text-center card-footer" style="color:#222;">Tên sản phẩm/5.000.000đ</p>
+            </div>           
+          </div>
+          
+        </div>
+        <div class="carousel-item">
+        <div class="row">
+            <div class="col-3">
+              <img src="/product/car2.jpg" class="img-thumbnail" alt="...">
+              <p class="text-center card-footer" style="color:#222;">Tên sản phẩm/5.000.000đ</p>
+            </div>
+            <div class="col-3">
+              <img src="/product/car2.jpg" class="img-thumbnail" alt="...">
+              <p class="text-center card-footer" style="color:#222;">Tên sản phẩm/5.000.000đ</p>
+            </div>
+            <div class="col-3">
+              <img src="/product/car2.jpg" class="img-thumbnail" alt="...">
+              <p class="text-center card-footer" style="color:#222;">Tên sản phẩm/5.000.000đ</p>
+            </div>
+            <div class="col-3">
+              <img src="/product/car2.jpg" class="img-thumbnail" alt="...">
+              <p class="text-center card-footer" style="color:#222;">Tên sản phẩm/5.000.000đ</p>
+            </div>           
+          </div>
+        </div>
+        
+      </div>
+      <button class="carousel-control-prev" type="button" data-target="#carouselExampleFade" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-target="#carouselExampleFade" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+  </div>
+  
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,146 +1,147 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+        <!-- Fonts -->
+        <link rel="dns-prefetch" href="//fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-danger shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <div id="app">
+            <nav class="navbar navbar-expand-md navbar-light bg-danger shadow-sm">
+                <div class="container">
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        {{ config('app.name', 'Laravel') }}
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <!-- Left Side Of Navbar -->
+                        <ul class="navbar-nav mr-auto">
 
-                    </ul>
+                        </ul>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
+                        <!-- Right Side Of Navbar -->
+                        <ul class="navbar-nav ml-auto">
+                            <!-- Authentication Links -->
+                            @guest
+                                @if (Route::has('login'))
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                    </li>
+                                @endif
 
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                @if (Route::has('register'))
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    </li>
+                                @endif
+                            @else
+                                <li class="nav-item dropdown">
+                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                        {{ Auth::user()->name }}
                                     </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                        document.getElementById('logout-form').submit();">
+                                            {{ __('Logout') }}
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                            @csrf
+                                        </form>
+                                    </div>
+                                </li>
+                            @endguest
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+
+            <!--second navbar-->
+            <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm navbar-hover">
+            
+                <a class="navbar-brand" href="#">
+                    
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHover" aria-controls="navbarDD" aria-expanded="false" aria-label="Navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarHover">                
+                    <ul class="container-fluid navbar-nav">
+                        @for ($i=0;$i<12;$i++)
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href=""
+                                data-toggle="dropdown_remove_dropdown_class_for_clickable_link" aria-haspopup="true"
+                                aria-expanded="false">
+                                Category
+                            </a>
+            
+                            <ul class="dropdown-menu">
+                                <li>
+                                    <a class="dropdown-item dropdown-toggle" href="">Subcategory(Computer)</a>
+            
+                                    <ul class="dropdown-menu">
+            
+                                        <li>
+                                            <a class="dropdown-item" href="">childcategory(Dell laptop)
+                                            </a>
+                                        </li>
+                                    </ul>        
+                                </li>       
+                            </ul>        
+                        </li>
+                        @endfor                    
                     </ul>
                 </div>
-            </div>
-        </nav>
+            </nav>
+            <!--end second navbar-->
 
-
-        <!--second navbar-->
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm navbar-hover">
-           
-            <a class="navbar-brand" href="#">
-                
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHover" aria-controls="navbarDD" aria-expanded="false" aria-label="Navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarHover">                
-                <ul class="container-fluid navbar-nav">
-                    @for ($i=0;$i<12;$i++)
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href=""
-                            data-toggle="dropdown_remove_dropdown_class_for_clickable_link" aria-haspopup="true"
-                            aria-expanded="false">
-                            Category
-                        </a>
-        
-                        <ul class="dropdown-menu">
-                            <li>
-                                <a class="dropdown-item dropdown-toggle" href="">Subcategory(Computer)</a>
-        
-                                <ul class="dropdown-menu">
-        
-                                    <li>
-                                        <a class="dropdown-item" href="">childcategory(Dell laptop)
-                                        </a>
-                                    </li>
-                                </ul>        
-                            </li>       
-                        </ul>        
-                    </li>
-                    @endfor                    
-                </ul>
-            </div>
-        </nav>
-        <!--end second navbar-->
-
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
-    <style>
-        /* .navbar li a {
-            color: white !important;
-        } */
-        
-        .dropdown:hover>.dropdown-menu {
-            display: block;
-        }
-        
-        /* hover dropdown menus */
-        @media only screen and (max-width: 991px) {
-        .navbar-hover .show > .dropdown-toggle::after{
-        transform: rotate(-90deg);
-        }
-        }
-        @media only screen and (min-width: 492px) {
-        
-        .navbar-hover .collapse ul li{position:relative;}
-        .navbar-hover .collapse ul li:hover> ul{display:block}
-        .navbar-hover .collapse ul ul{position:absolute;top:100%;left:0;min-width:250px;display:none}
-        .navbar-hover .collapse ul ul ul{position:absolute;top:0;left:100%;min-width:250px;display:none}
-        
+            <main class="py-4">
+                @yield('content')
+            </main>
+        </div>
+        <style>
+            /* .navbar li a {
+                color: white !important;
+            } */
+            
+            .dropdown:hover>.dropdown-menu {
+                display: block;
+            }
+            
+            /* hover dropdown menus */
+            @media only screen and (max-width: 991px) {
+            .navbar-hover .show > .dropdown-toggle::after{
+            transform: rotate(-90deg);
+            }
+            }
+            @media only screen and (min-width: 492px) {
+            
+            .navbar-hover .collapse ul li{position:relative;}
+            .navbar-hover .collapse ul li:hover> ul{display:block}
+            .navbar-hover .collapse ul ul{position:absolute;top:100%;left:0;min-width:250px;display:none}
+            .navbar-hover .collapse ul ul ul{position:absolute;top:0;left:100%;min-width:250px;display:none}
+            }
+            
         </style>
-</body>
+    </body>
 </html>
