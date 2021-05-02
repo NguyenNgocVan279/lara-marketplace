f60b48f8673665b03fdad1e57407debe643ca914
NguyenNgocVan279 <nguyenngocvan279@gmail.com>
2021-05-02 12:13:30 +0700

    L126.User password Change and Update

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -16,31 +16,34 @@
            
         </div> 
         <div class="col-md-3">
-            @if (session('error'))
-                <div class="alert alert-danger">
-                    {{ session('error') }}
-                </div>
+            @if (session('status') === 'password-updated')
+                <div class="alert alert-success">Đổi mật khẩu thành công!</div>
             @endif
-            @if (session('success'))
-                <div class="alert alert-success">
-                    {{ session('success') }}
-                </div>
-            @endif
-            <form action="" method="post">@csrf
+            <form action="{{route('user-password.update')}}" method="post">@csrf
+                @method('PUT')
                 <div class="card">
                     <div class="card-header text-white" style="background-color: red">Thay đổi mật khẩu</div>                    
                     <div class="card-body">
                         <div class="form-group">
                             <label>Mật khẩu hiện tại</label>
-                            <input type="password" name="current_password" class="form-controll">
+                            <input type="text" name="current_password" class="form-controll">
+                            @error('current_password')
+                                <div>{{ $message }}</div>    
+                            @enderror
                         </div>
                         <div class="form-group">
                             <label>Mật khẩu mới</label>
-                            <input type="password" name="new_password" class="form-controll">
+                            <input type="text" name="password" class="form-controll">
+                            @error('password')
+                                <div>{{ $message }}</div>    
+                            @enderror
                         </div>
                         <div class="form-group">
                             <label>Xác nhận mật khẩu</label>
-                            <input type="password" name="confirm_password" class="form-controll">
+                            <input type="text" name="password_confirmation" class="form-controll">
+                            @error('password_confirmation')
+                                <div>{{ $message }}</div>    
+                            @enderror
                         </div>
                         <div class="form-group">
                             <button type="submit" class="btn btn-danger">Cập nhật</button>                           
