1b979d922434d1c3507fb04d90bc6bbd516bee12
Nguyễn Ngọc Văn <nguyenngocvan@BCTC_GROUP.local>
2021-05-10 14:45:47 +0700

    L190.link sidebar for reported ads for admin

--- resources/views/backend/layouts/sidebar.blade.php
+++ resources/views/backend/layouts/sidebar.blade.php
@@ -61,9 +61,9 @@
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="pages/charts/chartjs.html">
+            <a class="nav-link" href="{{route('all.reported.ads')}}">
               <i class="mdi mdi-chart-pie menu-icon"></i>
-              <span class="menu-title">Charts</span>
+              <span class="menu-title">Tin đăng bị báo cáo</span>
             </a>
           </li>
           <li class="nav-item">
